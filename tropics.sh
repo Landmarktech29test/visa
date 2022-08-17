f08f2cd0043bf47cd1bc3d3ede500dc5a6980036
--Bola <--Bolnik2001@gmail.com>
2022-08-17 15:13:24 +0000

    my first commit

---
+++ tropics.sh
@@ -0,0 +1,5 @@
+echo  "we will start working on the tropical project for visa"
+echo " all developers must install git and github"
+echo "all hands on deck"
+echo "we will work in a scrum enviromnent and sprint for two weeks"
+
